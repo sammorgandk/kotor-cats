82fc44de36e3958cc14d88747abfed89c2686b86
sammorgandk <sammorgandk@gmail.com>
2023-09-04 05:41:06 -0400

    interview

--- recurse_center/app.py
+++ recurse_center/app.py
@@ -2,13 +2,34 @@
 # When your server receives a request on http://localhost:4000/set?somekey=somevalue it should store the passed key and value in memory. 
 # When it receives a request on http://localhost:4000/get?key=somekey it should return the value stored at somekey.
 
+# During your interview, you will pair on saving the data to a file. 
+# You can start with simply appending each write to the file, and work on making it more efficient if you have time.
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
-# data stored in memory
-key_value_pairs = {}
+# # data stored in memory
+# key_value_pairs = {}
 
+data_file = "data.txt"
+
+# writing data to file
+def write_to_file():
+    with open (data_file, "a") as f:
+        for key, value in data.items():
+            f.write(f"{key}={value}")
+        
+
+# function that reads data from file
+def read_to_file():
+    with open (data_file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            key, value = line.strip().split("=")
+            key_value_pairs[key] = value
+        return key_value_pairs
+        
 # route for set
 @app.route('/set')
 def set_key_value():
@@ -25,7 +46,7 @@ def set_key_value():
 def get_value():
     key = request.args.get('key')
     if key in key_value_pairs:
-        return key_value_pairs[key]
+        return f"The value '{key_value_pairs[key]}' is set to the key '{key}'"
     else:
         return f"Value for key '{key}' has not been set", 400
 
