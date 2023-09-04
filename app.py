397a3297838b42462d8af2352c5933140c53f965
sam <sammorgandk@gmail.com>
2023-09-04 07:47:35 -0400

    firebase

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost',port=5000)
