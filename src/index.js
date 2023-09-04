030f33e07157628e4ea000042ab92512565982c1
sam <sammorgandk@gmail.com>
2023-09-04 09:42:52 -0400

    blah

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Import functions
+import { initializeApp } from 'firebase/app'; 
+
+// App's Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyCb2I9d5MtV5ML45C7zRamB4tNL8mHAK3Q",
+    authDomain: "kotor-cats.firebaseapp.com",
+    projectId: "kotor-cats",
+    storageBucket: "kotor-cats.appspot.com",
+    messagingSenderId: "424119885132",
+    appId: "1:424119885132:web:3e10e69e4b0d1b21b843e9",
+    measurementId: "G-QRPHBRZ0CM"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
